b881cddb97d2521e91e7ac80642a4545a4296bda
Marcelo Lamaruggine <marcelolama.27@gmail.com>
2023-02-28 00:25:40 -0300

    agregamos productos, corregimos errores

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,28 +2,28 @@ import React from 'react'
 import './ItemListContainer.css'
 import Products from '../../Products.json'
 
-//const productos = Products
-
 const ItemListContainer = () => {
 
   const productos = Products
 
   return (
     <div className="container text-center productContainer">
-      <div className="row row-cols-3 cols justify-content-center">
+      <div className="row row-cols-4 cols justify-content-center">
         {
           productos.map(produ => (
-            <div className="col product">
-              <img src="" alt="">{produ.imagen}</img>
-              <h3 className='tituloProdu' >{produ.marca}</h3>
-              <p className='precioProdu'>{produ.precio}</p>
+            <div className="col product" key={produ.id}>
+              <img className='imagenProdu' src={produ.imagen} alt={produ.marca}/>
+              <h5 className='tituloProdu' >{produ.marca}</h5>
+              <p className='paisOrigen'>{produ.pais}</p>
+              <p className='precioProdu'><strong>${produ.precio}</strong></p>
+              <button className='btnAgregar'>+</button>
+              <button className='btnEliminar'>-</button>
             </div> 
             
           ))
         }
       </div>  
     </div>
-    //<div className='textGreeting'>{props.greeting}</div>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,21 +4,21 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-        <nav class="navbar navbar-expand-lg navbar-dark navbarMenu">
-          <div class="container-fluid">
-                <a class="navbar-brand" href="#">
+        <nav className="navbar navbar-expand-lg navbar-dark navbarMenu">
+          <div className="container-fluid">
+                <a className="navbar-brand" href="#">
                   <img src="../Logo-Pagina-Web.png" alt="logo" style={{width:'5rem'}}/>
                 </a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
               </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                  <a class="nav-link active" aria-current="page" href="#">Home</a>
-                  <a class="nav-link active" aria-current="page" href="#">Nuestra Historia</a>
-                  <a class="nav-link active" aria-current="page" href="#">Nuestras Marcas</a>
-                  <a class="nav-link active" aria-current="page" href="#">Nuestros Productos</a>
-                  <a class="nav-link active" aria-current="page" href="#">Donde Comprar</a>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  <a className="nav-link active" aria-current="page" href="#">Nuestra Historia</a>
+                  <a className="nav-link active" aria-current="page" href="#">Nuestras Marcas</a>
+                  <a className="nav-link active" aria-current="page" href="#">Nuestros Productos</a>
+                  <a className="nav-link active" aria-current="page" href="#">Donde Comprar</a>
                 </div>
             </div>
             <div>
