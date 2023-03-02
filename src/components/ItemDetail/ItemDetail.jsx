e29657d6df7a03028c0987d7cb0d4778ca8fb6df
Marcelo Lamaruggine <marcelolama.27@gmail.com>
2023-03-01 22:41:12 -0300

    agregamos ItemDetail.css

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import './ItemDetail.css'
 
 const ItemDetail = ({product}) => {
   return (
-    <div>
-      <img className='imagenProdu' src={product.imagen} alt={product.marca}/>
-      <h5 className='tituloProdu' >{product.marca}</h5>
-      <p className='paisOrigen'>{product.pais}</p>
-      <p className='precioProdu'><strong>${product.precio}</strong></p>
+    <div className='contenedorPrincipal'>
+        <div>
+            <img className='detailImagen' src={product.imagen} alt={product.marca}/>
+        </div>
+        <div className='contenedorInfo'>
+            <h5 className='detailTitulo' >{product.marca}</h5>
+            <p className='detailPais'>{product.pais}</p>
+            <p className='detailPrecio'><strong>${product.precio}</strong></p>
+        </div>
     </div>
   )
 }
